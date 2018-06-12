209bc01cbcb32e6a560b42cbc4959b2855d858dd
weian404 <weian404@gmail.com>
2018-06-12 14:14:53 +0800

    first commit

---
+++ src/main/java/com/example/demo/HelloController.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+
+    @RequestMapping("/")
+    public String hello(Model model){
+        model.addAttribute("msg","hello model");
+        return "hello";
+    }
+}
